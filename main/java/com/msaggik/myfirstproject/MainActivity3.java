0fb2635fc5fb252b2e57d094b11dc5463e77ecd0
esdrv <129773066+esdrv@users.noreply.github.com>
2023-08-11 22:43:13 +0700

    Add files via upload

---
+++ main/java/com/msaggik/myfirstproject/MainActivity3.java
@@ -0,0 +1,31 @@
+package com.msaggik.myfirstproject;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity3 extends AppCompatActivity {
+    private ImageButton imageButton;
+    private View.OnClickListener listner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageButton = findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(listner);
+    }
+    private View.OnClickListener listener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity4.class);
+        }
+    };
+}
